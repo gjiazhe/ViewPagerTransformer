bf79cc33f9779031804d1178275801385b2c3917
郭佳哲 <346672577@qq.com>
2016-12-05 12:26:22 +0800

    optimize flip transformer

--- viewpagertransformer/src/main/java/com/gjiazhe/viewpagertransformer/FlipPageTransformer.java
+++ viewpagertransformer/src/main/java/com/gjiazhe/viewpagertransformer/FlipPageTransformer.java
@@ -10,13 +10,9 @@ import android.view.View;
 public class FlipPageTransformer implements ViewPager.PageTransformer {
     @Override
     public void transformPage(View page, float position) {
-        // Counteract the default slide transition
-        page.setTranslationX(-position * page.getWidth());
-
-        // rotate
         final float rotation = 180 * position;
         page.setAlpha(Math.abs(rotation) > 90f ? 0 : 1);
-        page.setPivotX(page.getWidth() * 0.5f);
+        page.setPivotX(position < 0f ? page.getWidth() : 0f);
         page.setPivotY(page.getHeight() * 0.5f);
         page.setRotationY(rotation);
     }
